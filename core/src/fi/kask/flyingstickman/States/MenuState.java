77060dc65e9476bde73a6af4275f9d9d5c47e4c3
Alexander 'kask' Elo <alexander.elo@gmail.com>
2017-07-12 01:50:51 +0300

    Added background and play button for menu state

--- core/src/fi/kask/flyingstickman/States/MenuState.java
+++ core/src/fi/kask/flyingstickman/States/MenuState.java
@@ -1,14 +1,22 @@
 package fi.kask.flyingstickman.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import fi.kask.flyingstickman.FlyingStickman;
 
 /**
  * Created by kask on 12.7.2017.
  */
 public class MenuState extends State {
 
+    private Texture _backgroundTexture;
+    private Texture _playButtonTexture;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+
+        _backgroundTexture = new Texture("background.png");
+        _playButtonTexture = new Texture("playButton.png");
     }
 
     @Override
@@ -23,6 +31,11 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.begin();
 
+        sb.draw(_backgroundTexture, 0, 0, FlyingStickman.WIDTH, FlyingStickman.HEIGHT);
+        sb.draw(_playButtonTexture, FlyingStickman.WIDTH / 2 - _playButtonTexture.getWidth() / 2, 250);
+
+        sb.end();
     }
 }
