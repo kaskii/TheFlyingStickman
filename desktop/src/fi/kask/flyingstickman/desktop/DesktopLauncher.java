fe5301b740eaf8c031bca078f1ab42288db76351
Alexander 'kask' Elo <alexander.elo@gmail.com>
2017-07-12 01:06:43 +0300

    Empty LibGDX project

---
+++ desktop/src/fi/kask/flyingstickman/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.kask.flyingstickman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.kask.flyingstickman.FlyingStickman;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlyingStickman(), config);
+	}
+}
